7aeee3ffeb529d369b722301b14581e93c74cc96
Peter-G-Stone <peter.stone.online@gmail.com>
2018-11-18 10:28:15 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create({name: "annie", email: "anniemail", fav_icecream: "Dark Night from Springers"})
+User.create({name: "peter", email: "petermail", fav_icecream: "Chocolate w/ Chocolate"})
+User.create({name: "dad", email: "dadmail", fav_icecream: "black raspberry w chips"})
